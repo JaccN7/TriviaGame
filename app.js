9f032b3529ad9297abdda1d5593ceafafb435212
Javiera Cortés Celedón <javieracortesceledon@gmail.com>
2022-08-06 05:53:33 -0400

    app.js y package.json modificados

--- app.js
+++ app.js
@@ -4,34 +4,36 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
-//Morgan
-const morgan = require('morgan');
 
 //Importar rutas
-const playerScoreRoutes = require('./routes/playerScoreRoutes');
-const triviaQuestionRoutes = require('./routes/triviaQuestionRoutes');
-const userRoutes = require('./routes/userRoutes');
-const errorRoutes = require('./routes/errorRoutes');
+const playerScoreRoutes = require('./routes/api/playerScoreRoutes');
+const playerScoreWebRoutes = require('./routes/web/playerScoreWebRoutes');
+const triviaQuestionRoutes = require('./routes/api/triviaQuestionRoutes');
+const triviaQuestionWebRoutes = require('./routes/web/triviaQuestionWebRoutes');
+const userRoutes = require('./routes/api/userRoutes');
+const userWebRoutes = require('./routes/web/userWebRoutes');
+const errorRoutes = require('./routes/web/errorRoutes');
 
-app.use(morgan('dev'));
-
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // Contenido estatico de la aplicación
-app.use('/public', express.static(path.join(__dirname, './public')))
+app.use('/public', express.static(path.join(__dirname, './public')));
 
 //Motor de plantillas EJS
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-//Rutas
+//Rutas API
 app.use('/', playerScoreRoutes.routes);
-app.use('/game', triviaQuestionRoutes.routes);
-app.use('/adminprofile', userRoutes.routes);
+app.use('/api/game', triviaQuestionRoutes.routes);
+app.use('/api/adminprofile', userRoutes.routes);
+
+//Rutas Web
+app.use('/web/game', triviaQuestionWebRoutes.routes);
 app.use('/', errorRoutes.routes);
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}\nVisita: http://localhost:${PORT}\n______________________________________________`);
+    console.log(`Server is listening on port ${PORT}\nVisita: http://localhost:${PORT}\n______________________________________________\n\n`);
 });
