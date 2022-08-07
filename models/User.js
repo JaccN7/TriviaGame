f15e8c7a348d89e13fbcd095710cb49fe3da2c8f
Javiera Cortés Celedón <javieracortesceledon@gmail.com>
2022-08-07 19:48:38 -0400

    Se elimina el email de la clase user

--- models/User.js
+++ models/User.js
@@ -1,9 +1,8 @@
 class User {
     constructor(id, userEmail, userPassword, userName, userType, userStatus) {
         this.id = id;
-        this.userEmail = userEmail;
+        this.userName = userEmail;
         this.userPassword = userPassword;
-        this.userName = userName;
         this.userType = userType;
         this.userstatus = userStatus;
     }
