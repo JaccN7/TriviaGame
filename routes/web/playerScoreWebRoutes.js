2418e9aee8145a2af45ba8a028ce7593c16bd2bb
Javiera Cortés Celedón <javieracortesceledon@gmail.com>
2022-08-06 23:23:15 -0400

    se agrega ruta para guardar puntajes en playerScoreWebRoutes.js

---
+++ routes/web/playerScoreWebRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const { getPlayerScore, addPlayerScore } = require("../../controllers/playerScoreController");
+
+//Ruta para cargar tabla con puntajes
+router.get('/', getPlayerScore);
+
+//Ruta para ingresar nuevos puntajes
+router.post('/finishedtrivia', addPlayerScore);
+
+module.exports = {
+    routes: router
+};
