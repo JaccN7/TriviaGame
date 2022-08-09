0d55acfc71beeb16a488d45d97917170bfd9dd97
Javiera Cortés Celedón <javieracortesceledon@gmail.com>
2022-08-08 21:12:51 -0400

    middlewares actualizados

--- middleware/check_authentication.js
+++ middleware/check_authentication.js
@@ -1,13 +1,15 @@
 const jwt = require('jsonwebtoken');
 
-//Verficar que la session este activa recuperando los datos del objeto userIdentification 
 module.exports= (req, res, next) => {
+    let userName, userType;
+    req.session.userIdentification ? userName = req.session.userIdentification.userName : userName = "Anónimo";
+    req.session.userIdentification ? userType = req.session.userIdentification.userType : userType = "Invitado";
     try {
         const token = req.session.userIdentification.token;
         const decoded = jwt.verify(token, process.env.JWT_KEY);
         req.userData = decoded; 
         next();
     } catch (error) {
-        return res.render('error', { message: "Error al verificar la sessión", error: error.message});
+        return res.render('error', { message: "Error al verificar la sessión", error: error.message, userName: userName, userType: userType });
     }
 }
--- middleware/check_authorization.js
+++ middleware/check_authorization.js
@@ -1,15 +1,18 @@
 const jwt = require('jsonwebtoken');
 
-//verificar que el usuario sea administrador
+//Verificar permisos de usuario
 module.exports= (req, res, next) => {
+    let userName, userType;
+    req.session.userIdentification ? userName = req.session.userIdentification.userName : userName = "Anónimo";
+    req.session.userIdentification ? userType = req.session.userIdentification.userType : userType = "Invitado";
     try {
         const adminProfile = req.session.userIdentification.userType;
         if (adminProfile !== 'admin') {
-            return res.render('error', { message: "No tienes permisos para acceder a esta página", error: "No tiene acceso a este recurso" });
+            return res.render('error', { message: "No tienes permisos para acceder a esta página", error: "No tiene acceso a este recurso", userName: userName, userType: userType });
         } else {
             next();
         }
     } catch (error) {
-        return res.render('error', { message: "Error al verificar la sessión", error: error.message});
+        return res.render('error', { message: "Error al verificar la sessión", error: error.message, userName: userName, userType: userType});
     }
 }
